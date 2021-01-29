95bad4ce6b1461e5867844cf1650836c13df42b0
Pierrot Elie <epierrot002@iutbayonne.univ-pau.fr>
2021-01-29 10:44:26 +0100

    Message des cartes à jeter mis au clair Mettre un message d'erreur lorsque l'utilisateur se trompe de message

--- src/main/java/com/montaury/mus/jeu/joueur/InterfaceJoueurHumain.java
+++ src/main/java/com/montaury/mus/jeu/joueur/InterfaceJoueurHumain.java
@@ -10,6 +10,7 @@ import com.montaury.mus.jeu.tour.phases.dialogue.Kanta;
 import com.montaury.mus.jeu.tour.phases.dialogue.Paso;
 import com.montaury.mus.jeu.tour.phases.dialogue.Tira;
 import com.montaury.mus.jeu.tour.phases.dialogue.TypeChoix;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -17,50 +18,81 @@ import java.util.stream.Collectors;
 
 public class InterfaceJoueurHumain implements InterfaceJoueur {
 
-  private final Scanner scanner = new Scanner(System.in);
-  private Main main;
+    private final Scanner scanner = new Scanner(System.in);
+    private Main main;
 
-  @Override
-  public boolean veutAllerMus() {
-    println("Souhaitez-vous aller mus ? (o/n)");
-    return scanner.next().equals("o");
-  }
+    @Override
+    public boolean veutAllerMus() {
+        println("Souhaitez-vous aller mus ? (o/n)");
+        return scanner.next().equals("o");
+    }
 
-  @Override
-  public List<Carte> cartesAJeter() {
-    println("Veuillez saisir les cartes à jeter (ex: 1,3) :");
-    String aJeter = scanner.next();
-    return Arrays.stream(aJeter.split(","))
-      .mapToInt(Integer::parseInt)
-      .mapToObj(indiceCarte -> main.cartesDuPlusGrandAuPlusPetit().get(indiceCarte - 1))
-      .collect(Collectors.toList());
-  }
+    @Override
+    public List<Carte> cartesAJeter() {
+        println("Veuillez saisir les cartes à jeter (ex: 1,2,3 sachant que 1= Votre première carte) :");
+        String aJeter = scanner.next();
+        return Arrays.stream(aJeter.split(","))
+                .mapToInt(Integer::parseInt)
+                .mapToObj(indiceCarte -> main.cartesDuPlusGrandAuPlusPetit().get(indiceCarte - 1))
+                .collect(Collectors.toList());
+    }
 
-  @Override
-  public Choix faireChoixParmi(List<TypeChoix> choixPossibles) {
-    print("Faites un choix entre (en toutes lettres): ");
-    println(choixPossibles.stream().map(TypeChoix::nom).collect(Collectors.joining(" | ")));
-    String choix = scanner.next();
-    if (choix.equalsIgnoreCase("Paso")) return new Paso();
-    if (choix.equalsIgnoreCase("Imido")) return new Imido();
-    if (choix.equalsIgnoreCase("Hordago")) return new Hordago();
-    if (choix.equalsIgnoreCase("Idoki")) return new Idoki();
-    if (choix.equalsIgnoreCase("Tira")) return new Tira();
-    if (choix.equalsIgnoreCase("Gehiago")) return new Gehiago(1);
-    if (choix.equalsIgnoreCase("Kanta")) return new Kanta();
-    return new Paso();
-  }
+    @Override
+    public Choix faireChoixParmi(List<TypeChoix> choixPossibles) {
 
-  @Override
-  public void nouvelleMain(Main main) {
-    this.main = main;
-  }
+        print("Faites un choix entre (en toutes lettres): ");
 
-  private void println(String string) {
-    System.out.println(string);
-  }
+        while (true) {
+            println(choixPossibles.stream().map(TypeChoix::nom).collect(Collectors.joining(" | ")));
+            String choix = scanner.next();
 
-  private void print(String string) {
-    System.out.print(string);
-  }
+            /*if (choix !=  choixPossibles.stream().map(TypeChoix::nom).collect(Collectors.joining(" | ")))
+            {
+                print("Message incorrect ! Veuilez écrire les mots comme indiqué : ");
+            }
+            */
+            if (choix.equalsIgnoreCase("Paso")) {
+                return new Paso();
+            }
+            if
+            (choix.equalsIgnoreCase("Imido")) {
+                return new Imido();
+            }
+            if
+            (choix.equalsIgnoreCase("Hordago")) {
+                return new Hordago();
+            }
+            if
+            (choix.equalsIgnoreCase("Idoki")) {
+                return new Idoki();
+            }
+            if
+            (choix.equalsIgnoreCase("Tira")) {
+                return new Tira();
+            }
+            if
+            (choix.equalsIgnoreCase("Gehiago")) {
+                return new Gehiago(1);
+            }
+            if
+            (choix.equalsIgnoreCase("Kanta")) {
+                return new Kanta();
+            } else {
+                print("Message incorrect ! Veuilez écrire les mots comme indiqué : ");
+            }
+        }
+    }
+
+    @Override
+    public void nouvelleMain(Main main) {
+        this.main = main;
+    }
+
+    private void println(String string) {
+        System.out.println(string);
+    }
+
+    private void print(String string) {
+        System.out.print(string);
+    }
 }
